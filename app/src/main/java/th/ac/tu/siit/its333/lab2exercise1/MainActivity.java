33fb2577f7dfc6d972978aa948a0b2993d1f8a22
chanawat <best.zer@hotmail.com>
2015-01-30 16:08:25 +0700

    lab2exercise1
    calculator

--- app/src/main/java/th/ac/tu/siit/its333/lab2exercise1/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/its333/lab2exercise1/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
 
     // expr = the current string to be calculated
     StringBuffer expr;
-
+int mem = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +28,16 @@ public class MainActivity extends ActionBarActivity {
         tvExpr.setText(expr.toString());
     }
 
+
+    public void updateAnsDisplay(String s){
+        TextView tvAns = (TextView)findViewById(R.id.tvAns);
+        tvAns.setText(s);
+    }
+
+
+
+
+
     public void recalculate() {
         //Calculate the expression and display the output
 
@@ -36,6 +46,30 @@ public class MainActivity extends ActionBarActivity {
         //reference: http://stackoverflow.com/questions/2206378/how-to-split-a-string-but-also-keep-the-delimiters
         String e = expr.toString();
         String[] tokens = e.split("((?<=\\+)|(?=\\+))|((?<=\\-)|(?=\\-))|((?<=\\*)|(?=\\*))|((?<=/)|(?=/))");
+        if (!tokens[0].equals("")) {
+
+            double result;
+            result = Double.parseDouble(tokens[0]);
+            for (int i = 1; i < tokens.length - 1; i++) {
+                if (tokens[i].equals("+")) {
+                    result += Double.parseDouble(tokens[i + 1]);
+                } else if (tokens[i].equals("-")) {
+                    result -= Double.parseDouble(tokens[i + 1]);
+                } else if (tokens[i].equals("*")) {
+                    result *= Double.parseDouble(tokens[i + 1]);
+                } else if (tokens[i].equals("/")) {
+                    result /= Double.parseDouble(tokens[i + 1]);
+                }
+
+            }
+            updateAnsDisplay(Double.toString(result));
+        }
+    else
+        {
+
+            updateAnsDisplay(Double.toString(0));
+
+        }
     }
 
     public void digitClicked(View v) {
@@ -53,12 +87,23 @@ public class MainActivity extends ActionBarActivity {
         //IF the last character in expr is not an operator and expr is not "",
         //THEN append the clicked operator and updateExprDisplay,
         //ELSE do nothing
+        String d =((TextView)v).getText().toString();
+        if(!expr.toString().isEmpty() && (expr.charAt(expr.length()-1) != '/' ||
+                expr.charAt(expr.length()-1) != '*' || expr.charAt(expr.length()-1) != '+' ||expr.charAt(expr.length()-1) != '-'  ))
+        {
+            expr.append(d);
+            updateExprDisplay();
+            recalculate();
+        }
+
+
     }
 
     public void ACClicked(View v) {
         //Clear expr and updateExprDisplay
         expr = new StringBuffer();
         updateExprDisplay();
+        updateAnsDisplay("0");
         //Display a toast that the value is cleared
         Toast t = Toast.makeText(this.getApplicationContext(),
                 "All cleared", Toast.LENGTH_SHORT);
@@ -71,6 +116,47 @@ public class MainActivity extends ActionBarActivity {
             expr.deleteCharAt(expr.length()-1);
             updateExprDisplay();
         }
+    }
+    public void equalClicked(View v){
+        expr = new StringBuffer();
+
+        TextView tv = (TextView)findViewById(R.id.tvAns);
+        String s = tv.getText().toString();
+        expr.append(s);
+        updateExprDisplay();
+        updateAnsDisplay("0");
+
+    }
+    public void maddClicked(View v){
+        TextView tvAns = (TextView)findViewById(R.id.tvAns);
+        String s = tvAns.getText().toString();
+        mem += Double.parseDouble(s);
+        Toast t = Toast.makeText(this.getApplicationContext(),
+                mem + " has been added to mem",Toast.LENGTH_SHORT);
+        t.show();
+    }
+    public void mminusClicked(View v){
+        TextView tvAns = (TextView)findViewById(R.id.tvAns);
+        String s = tvAns.getText().toString();
+        mem -= Double.parseDouble(s);
+        Toast t = Toast.makeText(this.getApplicationContext(),
+                mem + " has been added to mem",Toast.LENGTH_SHORT);
+        t.show();
+    }
+    public void mrClicked(View v){
+        expr = new StringBuffer();
+        expr.append(Double.toString(mem));
+        updateExprDisplay();
+        updateAnsDisplay(Double.toString(mem));
+
+    }
+    public void mcClicked(View v){
+        expr = new StringBuffer();
+        updateExprDisplay();
+        mem=0;
+        updateAnsDisplay(Double.toString(mem));
+        Toast t = Toast.makeText(this.getApplicationContext(),"Mem Clear",Toast.LENGTH_SHORT);
+        t.show();
     }
 
     @Override
